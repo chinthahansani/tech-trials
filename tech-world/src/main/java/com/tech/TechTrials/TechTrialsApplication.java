cf7c0e51df38ec7386d452b7b128de3c3a7db0ae
chinthahansani <chinthahansani@gmail.com>
2024-08-09 03:45:28 +0300

    Initial Changes

---
+++ tech-world/src/main/java/com/tech/TechTrials/TechTrialsApplication.java
@@ -0,0 +1,13 @@
+package com.tech.TechTrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTrialsApplication.class, args);
+	}
+
+}
---
+++ tech-world/src/test/java/com/tech/TechTrials/TechTrialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.TechTrials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTrialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
